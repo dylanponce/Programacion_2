5925ceae2cb8a0b1e4de6e32df466cc25ebf7ce7
Dylan Ponce <dylan18ponce2323@gmail.com>
2026-08-14 16:38:29 -0300

    Add files via upload

--- Acumuladores/Acumuladores.java
+++ Acumuladores/Acumuladores.java
@@ -2,17 +2,13 @@ package acumuladores;
 
 public class Acumuladores {
 
-	/**
+	/*
 	 * Dada una matriz de enteros y un número, verifica si existe alguna fila 
 	 * donde todos sus elementos sean múltiplos del número recibido por 
 	 * parámetro.
-	 * 
+	 
 	 * Si la matriz está vacía o si el número no es positivo, devuelve falso.
-	 * 
-	 * @param mat
-	 * @param num
-	 * @return
-	*/
+	 */
 	
 	//Ejercicio 1
 	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) {
@@ -32,17 +28,13 @@ public class Acumuladores {
 		return ret;
 	}
 	
-	/**
+	/*
 	 * Dado 2 matrices se verifica si hay intersección entre las filas de cada
 	 * matriz, fila a fila.
-	 * 
+	 
 	 * Si las matrices tienen distinta cantidad de filas o si alguna matriz 
 	 * está vacía, devuelve falso.
-	 * 
-	 * @param mat1
-	 * @param mat2
-	 * @return
-	*/
+	 */
 	
 	//Ejercicio 2
 	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) {
@@ -67,18 +59,14 @@ public class Acumuladores {
 		return ret;
 	}
 	
-	/**
+	/*
 	 * Dada una matriz y el índice de una columna, se verifica si existe alguna
 	 * fila cuya suma de todos sus elementos sea mayor estricto que la suma de
 	 * todos los elementos de la columna indicada por parámetro.
-	 * 
+	  
 	 * Si el índice de la columna es inválido o la matriz está vacía, devuelve 
 	 * falso.
-	 * 
-	 * @param mat
-	 * @param nColum
-	 * @return
-	*/
+	 */
 	
 	//Ejercicio 3
 	public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int nColum) { 
@@ -105,17 +93,13 @@ public class Acumuladores {
 		return sumaColumna;
 	}
 	
-	/**
+	/*
 	 * Dadas 2 matrices, se verifica si hay intersección entre las columnas de
 	 * cada matriz, columna a columna.
-	 * 
+	 
 	 * Si las matrices tienen distinta cantidad de columnas o alguna matriz 
 	 * está vacía, devuelve falso. 
-	 * 
-	 * @param mat1
-	 * @param mat2
-	 * @return
-	*/
+	 */
 	
 	//Ejercicio 4
 	public boolean hayInterseccionPorColumna(int[][] mat1, int[][]mat2) {
@@ -139,3 +123,4 @@ public class Acumuladores {
 	    return existeComun;
 	}
 }
+
